1541a6c68b9ad569abcf947cff8623c4dcec93a7
Carolina Lunas <carolinalunasfarah@gmail.com>
2023-07-06 18:32:34 -0400

    items to style

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,51 +1,102 @@
-const propiedadesJSON = [
+let listProperties = [
     {
-      name: "Casa de campo",
-      description: "Un lugar ideal para descansar de la ciudad",
-      src:
-        "https://www.construyehogar.com/wp-content/uploads/2020/02/Dise%C3%B1o-casa-en-ladera.jpg",
-      rooms: 2,
-      m: 170
+        id: 1,
+        name: "Country house",
+        description: "Un lugar ideal para descansar de la ciudad",
+        src: "assets/img/2_countryhouse.jpg",
+        rooms: 2,
+        m: 170,
     },
     {
-      name: "Casa de playa",
-      description: "Despierta tus días oyendo el oceano",
-      src:
-        "https://media.chvnoticias.cl/2018/12/casas-en-la-playa-en-yucatan-2712.jpg",
-      rooms: 2,
-      m: 130
+        id: 2,
+        name: "Beach house",
+        description: "Despierta tus días oyendo el oceano",
+        image: "assets/img/5_beachhouse.jpg",
+        rooms: 4,
+        m: 130,
     },
     {
-      name: "Casa en el centro",
-      description: "Ten cerca de ti todo lo que necesitas",
-      src:
-        "https://fotos.perfil.com/2018/09/21/trim/950/534/nueva-york-09212018-366965.jpg",
-      rooms: 1,
-      m: 80
+        id: 3,
+        name: "Downtond house",
+        description: "Ten cerca de ti todo lo que necesitas",
+        image: "assets/img/3_townhouse.jpg",
+        rooms: 2,
+        m: 80,
     },
     {
-      name: "Casa rodante",
-      description: "Conviertete en un nómada del mundo sin salir de tu casa",
-      src:
-        "https://cdn.bioguia.com/embed/3d0fb0142790e6b90664042cbafcb1581427139/furgoneta.jpg",
-      rooms: 1,
-      m: 6
+        id: 4,
+        name: "Tailer",
+        description: "Conviertete en un nómada del mundo sin salir de tu casa",
+        image: "assets/img/4_trailer.jpg",
+        rooms: 1,
+        m: 6,
     },
     {
-      name: "Departamento",
-      description: "Desde las alturas todo se ve mejor",
-      src:
-        "https://www.adondevivir.com/noticias/wp-content/uploads/2016/08/depto-1024x546.jpg",
-      rooms: 3,
-      m: 200
+        id: 5,
+        name: "Apartment",
+        description: "Desde las alturas todo se ve mejor",
+        image: "assets/img/6_apartment.jpg",
+        rooms: 3,
+        m: 200,
     },
     {
-      name: "Mansión",
-      description: "Vive una vida lujosa en la mansión de tus sueños ",
-      src:
-        "https://resizer.glanacion.com/resizer/fhK-tSVag_8UGJjPMgWrspslPoU=/768x0/filters:quality(80)/cloudfront-us-east-1.images.arcpublishing.com/lanacionar/CUXVMXQE4JD5XIXX4X3PDZAVMY.jpg",
-      rooms: 5,
-      m: 500
+        id: 6,
+        name: "Mansion",
+        description: "Vive una vida lujosa en la mansión de tus sueños ",
+        image: "assets/img/1_mansion.jpg",
+        rooms: 5,
+        m: 500,
+    },
+];
+
+let inputs = Array.from(document.querySelectorAll("nav input"));
+
+let propertiesList = document.getElementById("propertiesList");
+let filter = document.querySelector(".filter");
+let count = document.getElementById("count");
+let search = document.getElementsByClassName("search");
+
+function showProperty(listProperties) {
+    count.innerHTML = listProperties.length;
+    propertiesList.innerHTML = "";
+
+    for (let item of listProperties) {
+        let newItem = document.createElement("div");
+        newItem.classList.add("item");
+        newItem.innerHTML = `
+      <div class = "item_container">
+        <article class = "item_image">
+          <img src= "${item.image}" alt="${item.name}">
+        </article>
+        <article class = "item_info">
+          <h5 class = "item_name">${item.name}</h5>
+          <p class = "item_description">${item.description}</p>
+        </article>
+        <article>
+          <p class = "item_rooms">Rooms: ${item.rooms}</p>
+          <p class = "item_m">Meters: ${item.m}</p>
+        </article>
+        <button class = "item_detailes">Details</button>
+      </div>
+      `;
+
+        propertiesList.appendChild(newItem);
     }
-  ];
-  
+}
+
+showProperty(listProperties);
+
+const filterProperties = () => {
+    let [{ value: rooms }, { value: smfrom }, { value: smto }] = inputs;
+    if (!rooms && !smfrom && !smto) {
+        alert("Please enter values");
+        return false;
+    }
+
+    let filterProperties = listProperties.filter(
+        (item) => item.rooms >= rooms && item.m >= smfrom && item.m >= smto
+    );
+    showProperty(filterProperties);
+};
+
+search.addEventListener("click", filterProperties);
